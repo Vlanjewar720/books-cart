c8b4cf8ca1b5ae4e2803dd2e57924ede3d728b99
Vaibhav <lanjewarvaibhav56@gmail.com>
2023-06-21 16:55:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cart from './book_component/Cart';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Cart/>
+    </div>
+  );
+}
+
+export default App;
